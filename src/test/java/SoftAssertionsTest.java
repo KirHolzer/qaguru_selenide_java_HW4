b34db42ff8f2209e9050d2c2a9697fe90a6887ff
Кирилл Гольцер <kirill.holzer@yandex.ru>
2024-09-17 02:37:01 +0300

    final test for - ready to merge

---
+++ src/test/java/SoftAssertionsTest.java
@@ -0,0 +1,36 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SoftAssertionsTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+    @Test
+    void jUnitSoftAssertions() {
+        open("/selenide/selenide"); //Откройте страницу Selenide в Github
+        $("#wiki-tab").click();
+        $(".markdown-body").$("[href='/selenide/selenide/wiki/SoftAssertions']").click();
+        $("#wiki-body").shouldHave(text(
+                """
+                        @ExtendWith({SoftAssertsExtension.class})
+                        class Tests {
+                          @Test
+                          void test() {
+                            Configuration.assertionMode = SOFT;
+                            open("page.html");
+                                                
+                            $("#first").should(visible).click();
+                            $("#second").should(visible).click();
+                          }
+                        }
+                """
+                ));
+        //sleep(5000);
+    }
+}
