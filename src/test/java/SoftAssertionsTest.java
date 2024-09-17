5e9d0af0e648262cd602b9d37914e935c3dda4a3
Кирилл Гольцер <kirill.holzer@yandex.ru>
2024-09-17 12:56:59 +0300

    after review + added second test and refactor

--- src/test/java/SoftAssertionsTest.java
+++ src/test/java/SoftAssertionsTest.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SoftAssertionsTest {
@@ -10,14 +13,13 @@ public class SoftAssertionsTest {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://github.com";
         Configuration.pageLoadStrategy = "eager";
+
     }
-    @Test
-    void jUnitSoftAssertions() {
-        open("/selenide/selenide"); //Откройте страницу Selenide в Github
-        $("#wiki-tab").click();
-        $(".markdown-body").$("[href='/selenide/selenide/wiki/SoftAssertions']").click();
-        $("#wiki-body").shouldHave(text(
-                """
+    @AfterEach
+    void closeBrowser() {
+        Selenide.closeWebDriver(); // Закрывает браузер после каждого теста
+    }
+    String snippetOfJUnit5 =  """
                         @ExtendWith({SoftAssertsExtension.class})
                         class Tests {
                           @Test
@@ -29,8 +31,26 @@ public class SoftAssertionsTest {
                             $("#second").should(visible).click();
                           }
                         }
-                """
-                ));
+                """;
+    @Test
+    void jUnitSoftAssertions() {
+        open("/selenide/selenide"); //Откройте страницу Selenide в Github
+        $("#wiki-tab").click();
+        // проверка -  что страница SoftAssertions есть в списке Pages
+        $(".markdown-body").shouldHave(text("Soft assertions"));
+        $(byText("Soft assertions")).click();
+        sleep(5000);
+        $("#wiki-body").shouldHave(text(snippetOfJUnit5));
         //sleep(5000);
+    }
+
+    // добавлена проверка с правого сайдбара
+    @Test
+    void jUnitSoftAssertionsSidebar(){
+        open("/selenide/selenide");
+        $("#wiki-tab").click();
+        $(".wiki-rightbar").$(byText("Show 3 more pages…")).click();
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text(snippetOfJUnit5));
     }
 }
